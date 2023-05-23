323ddd01b6820427fdd8bd84a3794b0cf84c507c
Annetjepkorirr <124133670+Annetjepkorirr@users.noreply.github.com>
2023-05-23 21:32:06 +0300

    Update review.py

--- review.py
+++ review.py
@@ -3,18 +3,18 @@ class Review:
         self.customer_name = customer_name
         self.rating = rating
         self.comment = comment
-
     def display_review(self):
         print(f"Customer: {self.customer_name}")
         print(f"Rating: {self.rating}")
         print(f"Comment: {self.comment}")
-      
-        
-        
         # Create a review object
 customer_review = Review("Jirani", 3.5, "Great quality fruits and vegetables!")
-
 # Display the review details
 customer_review.display_review()
-        
-        
+
+
+
+
+
+
+
